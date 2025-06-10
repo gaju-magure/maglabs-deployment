77a1fd85657267f1a33cc76300a300801105f957
gaju-magure <gajanand@magureinc.com>
2025-06-10 08:31:31 +0530

    improve(magure-app): refactor frontend removed useless component

--- pocs/magure-app/frontend/src/pages/SuperAdmin/UsersPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { UserManagementTable } from '@/components/common/UserManagementTable';
-import { UserRole } from '@/enums/userRole';
-
-export const UsersPage: React.FC = () => (
-  <div className="space-y-6">
-    <UserManagementTable role={UserRole.SuperAdmin} />
-  </div>
-);
